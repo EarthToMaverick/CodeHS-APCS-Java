30a42b1afa48246853e5dc9f3d4f71d0b6b9762d
Jarett Clark <55763742+EarthToMaverick@users.noreply.github.com>
2020-08-28 10:08:34 -0700

    Update 2.13.4 Divisibility.java
    changed it so it actually works.

--- Unit 2/2.13.4 Divisibility.java
+++ Unit 2/2.13.4 Divisibility.java
@@ -16,18 +16,20 @@ public class Divisibility extends ConsoleProgram
     {
         int dividend = readInt("Enter the dividend: ");
         int divisor = readInt("Enter the divisor: ");
-        if(divisor != 0){
-            System.out.println(dividend +" is not divisible by " + divisor + "!");
-        }
-        else{
-            if(dividend / divisor == (double) dividend / divisor)
+        if(divisor == 0)
         {
-            System.out.println(dividend + " is divisible by " + divisor + "!");
+            System.out.println(dividend +" is not divisible by " + divisor);
         }
         else
         {
-            System.out.println(dividend + " is not divisible by " + divisor);
-        }
+            if(dividend / divisor == (double) dividend / divisor)
+            {
+            System.out.println(dividend + " is divisible by " + divisor + "!");
+            }
+            else
+            {  
+                System.out.println(dividend + " is not divisible by " + divisor);
+            }
         }
         
     }
